4d833bb48ae59fc78407c943763c5cf6ea9629fc
serisbu <seris@bu.edu>
2026-02-19 18:48:41 -0500

    Update exercise 1 to create data json

--- fda-api-test.py
+++ fda-api-test.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 #  As a user, I want to be able to see if the drug I am looking for is in
@@ -10,5 +11,7 @@ response = requests.get("https://api.fda.gov/drug/shortages.json?limit=100")
 if response.status_code == 200:
     data = response.json()
     print(data)
+    with open('data.json', 'w') as outf:
+        json.dump(data, outf, indent=4)
 else:
     print(f"Error: {response.status_code}")
