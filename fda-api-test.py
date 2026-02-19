c802ec7932d741b46677d3c3d4d101bca3dbf4ca
serisbu <seris@bu.edu>
2026-02-19 18:07:54 -0500

    Add to request library fda api test

---
+++ fda-api-test.py
@@ -0,0 +1,14 @@
+import requests
+
+
+#  As a user, I want to be able to see if the drug I am looking for is in
+#  shortage, so that I can make informed decisions about my healthcare.
+
+response = requests.get("https://api.fda.gov/drug/shortages.json?limit=100")
+
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
